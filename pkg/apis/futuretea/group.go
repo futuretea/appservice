8498a5a9e0bc1bd38fa97f3ea553940a30c3b6c5
futuretea <1913508671@qq.com>
2019-09-16 17:58:35 +0800

    init code

---
+++ pkg/apis/futuretea/group.go
@@ -0,0 +1,6 @@
+// Package futuretea contains futuretea API versions.
+//
+// This file ensures Go source parsers acknowledge the futuretea package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package futuretea
---
+++ pkg/apis/futuretea/v1/doc.go
@@ -0,0 +1,4 @@
+// Package v1 contains API Schema definitions for the futuretea v1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=futuretea.github.io
+package v1
